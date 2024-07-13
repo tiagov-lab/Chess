53924cf2fbb3ff68e9fbfa1f239b76c3c04a6c53
Tiago V <tiagovasquez98@gmail.com>
2024-07-13 15:11:24 +0200

    Wrote switch statement for Piece unicode.

--- Chess/Piece.cs
+++ Chess/Piece.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Text;
@@ -13,6 +14,8 @@ namespace Chess
         public (int X, int Y) Position { get; set; }
         public Colour Color { get; set; }
         public PieceType Type { get; set; }
+        
+        public Dictionary<PieceType, char> Colours { get; set; }
 
         public enum Colour
         {
@@ -42,13 +45,38 @@ namespace Chess
             Position = (x, y);
         }
 
+
         public override string ToString()
         {
-              // If white
-                // Switch statement
-                // "case Rook: return ♖" etc
+            switch (this.Color)
+            {
+                case Colour.White:
+                    switch (this.Type)
+                    {
+                        case PieceType.King: return "\u2654";
+                        case PieceType.Queen: return "\u2655";
+                        case PieceType.Rook: return "\u2656";
+                        case PieceType.Bishop: return "\u2657";
+                        case PieceType.Knight: return "\u2658";
+                        case PieceType.Pawn: return "\u2659";
+                        default: return "?";
+                    }
 
-            return "♖";
+                case Colour.Black:
+                    switch (this.Type)
+                    {
+                        case PieceType.King: return "\u265A";
+                        case PieceType.Queen: return "\u265B";
+                        case PieceType.Rook: return "\u265C";
+                        case PieceType.Bishop: return "\u265D";
+                        case PieceType.Knight: return "\u265E";
+                        case PieceType.Pawn: return "\u265F";
+                        default: return "?";
+                    }
+
+                default:
+                    return "?";
+            }
         }
     }
 }
