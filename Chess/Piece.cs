de211056925f1b4a6846dac516a377a75dde47c3
Tiago V <tiagovasquez98@gmail.com>
2024-07-13 11:38:15 +0200

    Defined Piece

--- Chess/Piece.cs
+++ Chess/Piece.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static Chess.Piece;
 
 namespace Chess
 {
-    internal class Piece
+    public class Piece
     {
+        public (int X, int Y) Position { get; set; }
+        public Colour Color { get; set; }
+        public PieceType Type { get; set; }
+
+        public enum Colour
+        {
+            White,
+            Black
+        }
+
+        public enum PieceType
+        {
+            Pawn,
+            Rook,
+            Knight,
+            Bishop,
+            Queen,
+            King
+        }
+
+        public Piece(int x, int y, Colour color, PieceType type)
+        {
+            Position = (x, y);
+            Color = color;
+            Type = type;
+        }
+
+        public void MoveTo(int x, int y)
+        {
+            Position = (x, y);
+        }
     }
 }
--- Chess/ValidMoveFinder.cs
+++ Chess/ValidMoveFinder.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Chess
 {
-    internal class ValidMoveFinder
+    public static class ValidMoveFinder
     {
     }
 }
