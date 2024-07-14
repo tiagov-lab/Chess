d237c138bef340435fc48a00ddd53f760d0d6fc6
Tiago V <tiagovasquez98@gmail.com>
2024-07-14 12:01:42 +0200

    Outlined Gameloop

---
+++ Chess/Gameloop.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess
+{
+    public static class Gameloop
+    {
+        public static void Start()
+        {
+            // Init board
+            Board gameBoard = new Board();
+
+            // Create players?
+            
+            //
+            // while (true) 
+            {
+                // white moves
+                gameBoard.displayBoard();
+
+                // black moves
+                gameBoard.displayBoard();
+
+                // Checkmate or resigning break the loop
+
+            }
+
+            // Insert data to DB
+        }
+    }
+}
--- Chess/MovementStrategy/PawnMoveStrategy.cs
+++ Chess/MovementStrategy/PawnMoveStrategy.cs
@@ -10,6 +10,12 @@ namespace Chess.MovementStrategy
     {
         public List<(int, int)> GetPossibleMoves(Piece piece, Board board)
         {
+            // Check if rook is in first space
+             // Take colour into account
+            //
+             // Check if there is a piece in tile ahead, break loop
+             // If there isn't, check if there is an enemy piece in diagonal spaces
+               // Add those spaces to list
             throw new NotImplementedException();
         }
     }
