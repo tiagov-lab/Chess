6c3f2a125b5d88dd8345184115fd77a2e8043ab0
Tiago Vasquez <tiagovasquez98@gmail.com>
2024-08-09 21:13:09 +0200

    Add MoveUtils

--- Chess/MovementStrategy/MoveUtils.cs
+++ Chess/MovementStrategy/MoveUtils.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Chess;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,46 @@ using System.Threading.Tasks;
 
 public static class MoveUtils
 {
+    public static void MovePiece(Board inputBoard, Piece inputPiece, Coordinate inputCoordinate)
+    {
+        // Remove Piece from current cell 
+        inputBoard.Cells[inputPiece.Pos.X, inputPiece.Pos.Y].RemovePiece();
 
+        // Add Piece to new cell
+        inputBoard.Cells[inputCoordinate.X, inputCoordinate.Y].PlacePiece(inputPiece);
+    }
+
+    public static bool canMoveToCell(Board inputBoard, Piece inputPiece, Coordinate inputCoordinate)
+    {
+        Cell movingToCell = inputBoard.Cells[inputCoordinate.X, inputCoordinate.Y];
+
+        if (inputBoard.IsValidPosition(inputCoordinate.X, inputCoordinate.Y))
+        {
+            if (movingToCell.isOccupied)
+            {
+                if (movingToCell.Piece.Color == inputPiece.Color)
+                {
+                    return false;
+                }
+
+                else
+                {
+                    return true;
+                }
+
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        else
+        {
+            return false;
+        }
+
+    }
 }
+
