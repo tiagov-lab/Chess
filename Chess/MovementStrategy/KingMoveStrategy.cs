9833db031800170965488921f10ab21241bdef75
Tiago Vasquez <tiagovasquez98@gmail.com>
2024-08-10 16:48:47 +0200

    Add KingMoveStrategy with Check functionality.

--- Chess/MovementStrategy/KingMoveStrategy.cs
+++ Chess/MovementStrategy/KingMoveStrategy.cs
@@ -10,7 +10,56 @@ namespace Chess.MovementStrategy
     {
         public List<Coordinate> GetPossibleMoves(Piece piece, Board board)
         {
-            throw new NotImplementedException();
+            List<Coordinate> possibleMoves = new List<Coordinate>();
+            List<Coordinate> enemyPossibleMoves = GetEnemyPossibleMoves(piece, board);
+
+            int x = piece.Position.X;
+            int y = piece.Position.Y;
+
+
+            // All possible king moves
+            Coordinate[] kingMoves = new Coordinate[]
+            {
+                new Coordinate(x + 1, y + 2),
+                new Coordinate(x + 2, y + 1),
+                new Coordinate(x + 2, y - 1),
+                new Coordinate(x + 1, y - 2),
+                new Coordinate(x - 1, y - 2),
+                new Coordinate(x - 2, y - 1),
+                new Coordinate(x - 2, y + 1),
+                new Coordinate(x - 1, y + 2)
+            };
+
+            // Remove moves where king would be in check
+            var kingMovesMinusEnemyMoves = kingMoves.Except(enemyPossibleMoves);
+
+            foreach (Coordinate move in kingMovesMinusEnemyMoves)
+            {
+                if (MoveUtils.canMoveToCell(board, piece, move))
+                {
+                    possibleMoves.Add(move);
+                }
+            }
+            return possibleMoves;
+        }
+
+        private List<Coordinate> GetEnemyPossibleMoves(Piece piece, Board inputBoard)
+        {
+            List<Coordinate> possibleMoves = new List<Coordinate>();
+
+            var EnemyPieces =
+                from p in Piece.PieceList
+                where p.Color != piece.Color
+                select p;
+
+            foreach (var p in EnemyPieces)
+            {
+                var tempMoves = p.GetPossibleMoves(inputBoard);
+
+                possibleMoves.AddRange(tempMoves);
+            }
+
+            return possibleMoves;
         }
     }
 }
--- Chess/MovementStrategy/KnightMoveStrategy.cs
+++ Chess/MovementStrategy/KnightMoveStrategy.cs
@@ -7,25 +7,33 @@ namespace Chess.MovementStrategy
         public List<Coordinate> GetPossibleMoves(Piece piece, Board board)
         {
             List<Coordinate> possibleMoves = new List<Coordinate>();
+
             int x = piece.Position.X;
             int y = piece.Position.Y;
 
             // All possible knight moves
             Coordinate[] knightMoves = new Coordinate[]
             {
-                new Coordinate(x + 1, y + 2),
-                new Coordinate(x + 2, y + 1),
-                new Coordinate(x + 2, y - 1),
-                new Coordinate(x + 1, y - 2),
-                new Coordinate(x - 1, y - 2),
-                new Coordinate(x - 2, y - 1),
-                new Coordinate(x - 2, y + 1),
-                new Coordinate(x - 1, y + 2)
+                // Top left
+                new Coordinate(x - 1, y + 1),
+                // Top
+                new Coordinate(x, y + 1),
+                // Top right
+                new Coordinate(x + 1, y + 1),
+                // Left
+                new Coordinate(x - 1, y),
+                // Right
+                new Coordinate(x + 1, y),
+                // Bottom left
+                new Coordinate(x - 1, y - 1),
+                // Bottom
+                new Coordinate(x, y - 1),
+                // Bottom right
+                new Coordinate(x + 1, y - 1),
             };
 
             foreach (Coordinate move in knightMoves)
             {
-
                 if (MoveUtils.canMoveToCell(board, piece, move))
                 {
                     possibleMoves.Add(move);
