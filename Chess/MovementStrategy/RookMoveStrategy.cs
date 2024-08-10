7495cd8c4d5396c2179d89d645d69f00a3d1a76f
Tiago Vasquez <tiagovasquez98@gmail.com>
2024-08-10 21:01:45 +0200

    Add RookMoveStrategy.

--- Chess/MovementStrategy/RookMoveStrategy.cs
+++ Chess/MovementStrategy/RookMoveStrategy.cs
@@ -10,7 +10,29 @@ namespace Chess.MovementStrategy
     {
         public List<Coordinate> GetPossibleMoves(Piece piece, Board board)
         {
-            throw new NotImplementedException();
+            List<Coordinate> possibleMoves = new List<Coordinate>();
+
+            int x = piece.Position.X;
+            int y = piece.Position.Y;
+
+            // All Queen directions
+            Coordinate[] rookDirections = new Coordinate[]
+            {
+                // Top
+                new Coordinate(x, y + 1),
+                // Left
+                new Coordinate(x - 1, y),
+                // Right
+                new Coordinate(x + 1, y),
+                // Bottom
+                new Coordinate(x, y - 1),
+            };
+
+            foreach (Coordinate direction in rookDirections)
+            {
+                possibleMoves.AddRange(MoveUtils.getMovesInDirection(board, piece, direction));
+            }
+            return possibleMoves;
         }
     }
 }
